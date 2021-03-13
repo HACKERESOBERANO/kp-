d53b4042ad4b285db8b2887afe9ff61797ee3d06
HACKERESOBERANO <78027781+HACKERESOBERANO@users.noreply.github.com>
2021-03-13 17:55:46 -0300

    Add files via upload

---
+++ dark/gcard.js
@@ -0,0 +1,18 @@
+[
+    {
+	"teks": "*CARTÃO GERADO:* 5500 2503 0518 5576 *VALIDADE:* 08/26 *CVV:* 860",
+	"image": "https://i.ibb.co/d6xFVKs/Pics-Art-03-11-06-33-50.jpg"
+    },
+    {
+	"teks": "*CARTÃO GERADO:* 5500 2503 0518 5576 *VALIDADE:* 08/26 *CVV:* 860",
+	"image": "https://i.ibb.co/d6xFVKs/Pics-Art-03-11-06-33-50.jpg"
+    },
+    {
+	"teks": "*CARTÃO GERADO:* 5427 6023 2775 3080 *VALIDADE:* 21/11/2021 *CVV:* 621",
+	"image": "https://i.ibb.co/TPS2TRn/Pics-Art-03-11-06-39-47.jpg"
+    },
+    {
+    "teks": "*CARTÃO GERADO:* 5464791355100712 *VALIDADE:* 08/26 *CVV:* 860",
+    "image": "https://i.ibb.co/HrHHhsC/Pics-Art-03-11-06-50-47.jpg"
+    }
+]
---
+++ dark/gcpf2.js
@@ -0,0 +1,114 @@
+[
+    {
+	"teks": "CPF GERADO: 884.506.490-56",
+	"image": "https://i.ibb.co/wyHw9KG/Pics-Art-03-11-07-09-45.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 974.796.180-69",
+	"image": "https://i.ibb.co/GvTZ7rW/Pics-Art-03-11-07-10-48.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 187.616.300-39",
+	"image": "https://i.ibb.co/L1vrn5h/Pics-Art-03-11-07-12-35.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 429.449.510-33",
+	"image": "https://i.ibb.co/v3PvB4h/Pics-Art-03-11-07-13-10.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 315.970.360-64",
+    "image": "https://i.ibb.co/5Y2GCxX/Pics-Art-03-11-07-13-49.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 170.609.870-70",
+    "image": "https://i.ibb.co/sWvJXd6/Pics-Art-03-11-07-14-28.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 171.618.810-56",
+    "image": "https://i.ibb.co/bsyjsYq/Pics-Art-03-11-07-42-21.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 647.518.770-46",
+    "image": "https://i.ibb.co/M8JSTbC/Pics-Art-03-11-07-42-57.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 278.383.240-25",
+    "image": "https://i.ibb.co/QnnGVxT/Pics-Art-03-11-07-43-21.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 400.138.290-33",
+    "image": "https://i.ibb.co/njVNMnX/Pics-Art-03-11-07-43-40.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 791.164.510-82",
+    "image": "https://i.ibb.co/NxNsjWb/Pics-Art-03-11-07-44-00.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 483.059.070-07",
+    "image": "https://i.ibb.co/Phsgvbv/Pics-Art-03-11-07-44-18.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 707.888.020-03",
+    "image": "https://i.ibb.co/y8GGxbY/Pics-Art-03-11-07-39-52.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 239.006.510-43",
+    "image": "https://i.ibb.co/cLPkycV/Pics-Art-03-11-07-45-37.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 884.506.490-56",
+	"image": "https://i.ibb.co/wyHw9KG/Pics-Art-03-11-07-09-45.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 974.796.180-69",
+	"image": "https://i.ibb.co/GvTZ7rW/Pics-Art-03-11-07-10-48.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 187.616.300-39",
+	"image": "https://i.ibb.co/L1vrn5h/Pics-Art-03-11-07-12-35.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 429.449.510-33",
+	"image": "https://i.ibb.co/v3PvB4h/Pics-Art-03-11-07-13-10.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 315.970.360-64",
+    "image": "https://i.ibb.co/5Y2GCxX/Pics-Art-03-11-07-13-49.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 170.609.870-70",
+    "image": "https://i.ibb.co/XYZkGr3/Pics-Art-03-11-07-14-28.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 171.618.810-56",
+    "image": "https://i.ibb.co/SfrVQc8/Pics-Art-03-11-07-15-04.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 647.518.770-46",
+    "image": "https://i.ibb.co/JKSwHwT/Pics-Art-03-11-07-37-36.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 278.383.240-25",
+    "image": "https://i.ibb.co/VMxDwLk/Pics-Art-03-11-07-38-06.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 400.138.290-33",
+    "image": "https://i.ibb.co/XbymJP8/Pics-Art-03-11-07-38-34.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 791.164.510-82",
+    "image": "https://i.ibb.co/ZY2Fy2C/Pics-Art-03-11-07-39-02.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 483.059.070-07",
+    "image": "https://i.ibb.co/JHC2vp9/Pics-Art-03-11-07-39-30.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 707.888.020-03",
+    "image": "https://i.ibb.co/y8GGxbY/Pics-Art-03-11-07-39-52.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 239.006.510-43",
+    "image": "https://i.ibb.co/SN4kwNR/Pics-Art-03-11-07-40-13.jpg"
+    }
+]
