c4bedbb75488584576ccd59ed58bdd2754ed35b0
HACKERESOBERANO <78027781+HACKERESOBERANO@users.noreply.github.com>
2021-03-13 17:54:22 -0300

    Add files via upload

---
+++ dark/bash/nodejs13/antilag.js
@@ -0,0 +1,14 @@
+const antilag = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Dark Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no githube: markoswythor*
+
+⧿⧽ *Não se esqueça de seguir*
+⧿⧽ *manda mensagem ae pow*
+⧿⧽ *Atenciosamente Markos*
+
+`
+}
+exports.antilag = antilag
---
+++ dark/bash/nodejs13/antispam.js
@@ -0,0 +1,14 @@
+const antispam = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Markos Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do git hub : markoswythor*
+
+⧿⧽ *Não se esqueça de seguir*
+⧿⧽ *manda msg lá*
+⧿⧽ *Atenciosamente Markos*
+
+`
+}
+exports.antispam = antispam
